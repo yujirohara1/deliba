0a4e92186f0c0e9ade5a5deef45af0d8e33b1ae1
yujirohara <yujirohara1@gmail.com>
2021-01-03 14:27:39 +0900

    Signed-off-by: yujirohara <yujirohara1@gmail.com>

--- app.py
+++ app.py
@@ -1,18 +1,44 @@
-#coding:utf-8
+from flask import Flask, render_template, g, request, redirect, url_for # 変更
+from hamlish_jinja import HamlishExtension
+# from werkzeug import ImmutableDict
+from werkzeug.datastructures import ImmutableDict
+import os
+from flask_sqlalchemy import SQLAlchemy # 変更
 
-from flask import Flask, render_template
+class FlaskWithHamlish(Flask):
+    jinja_options = ImmutableDict(
+        extensions=[HamlishExtension]
+    )
+app = FlaskWithHamlish(__name__)
 
-app = Flask(__name__)
+# app = Flask(__name__)
+
+db_uri = "sqlite:///" + os.path.join(app.root_path, 'milk.db') # 追加
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri # 追加
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app) # 追加
+
+class Item(db.Model): # 追加
+    __tablename__ = "item" # 追加
+    id = db.Column(db.Integer, primary_key=True) # 追加
+    code = db.Column(db.String(), nullable=False) # 追加
+    name1 = db.Column(db.String(), nullable=False) # 追加
 
 @app.route('/')
-def index():
-    return "Hallo World!"
+def hello_world():
+    entries = Item.query.all() #変更
+    return render_template('index.haml', entries=entries)
 
-if __name__ == '__main__':
-    app.debug = True
-    app.run()
+@app.route('/post', methods=['POST'])
+def add_entry():
+    entry = Item()
+    entry.code = request.form['code']
+    entry.name1 = request.form['name1']
+    db.session.add(entry)
+    db.session.commit()
+    return redirect(url_for('hello_world'))
 
-#デバッグモードTrueにすると変更が即反映される
-#ファイルのエンコードはUTF-8で保存すること
-#下記URLをブラウザに打ち込むとページが開く
-# http://127.0.0.1:5000/
+
+## おまじない
+if __name__ == "__main__":
+    app.run(debug=True)
