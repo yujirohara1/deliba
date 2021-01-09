00e6be0f6f8c144b8fea29b14d57ad030292d70a
yujirohara <yujirohara1@gmail.com>
2021-01-09 17:47:42 +0900

    Signed-off-by: yujirohara <yujirohara1@gmail.com>

--- static/main/js/main.js
+++ static/main/js/main.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
   });
   
   createCustomerTables_Main();
-  
+  createDaichoTables_Main(0);
   
   //var domTableCustomer = $('#tableCustomer').DataTable();
 
@@ -24,53 +24,9 @@ $(document).ready(function() {
   });
  
   $('#tableCustomer tbody').on( 'click', 'tr', function () {
-  
     //顧客テーブルから指定したレコード
     var rowData =   $('#tableCustomer').DataTable().row( this ).data();
-    
-    //台帳データテーブルを作成
-    $("#tableDaicho").DataTable({
-        bInfo: true,
-        bSort: false,
-        destroy: true,
-        "processing": true,
-        ajax: {
-            url: "/getVDaichoA_ByCusotmerId/" + rowData.id + "",
-            dataType: "json",
-            dataSrc: function ( json ) {
-                return JSON.parse(json.data);
-            },
-            contentType:"application/json; charset=utf-8",
-            complete: function () {
-                return; 
-            }
-        },
-        columns: [
-            { data: 'item_id'     ,width: '12%'},
-            { data: 'iname1'      ,width: '41%'},
-            { data: 'tanka'       ,width: '12%'},
-            { data: 'getu'        ,width: '5%'},
-            { data: 'ka'          ,width: '5%'},
-            { data: 'sui'         ,width: '5%'},
-            { data: 'moku'        ,width: '5%'},
-            { data: 'kin'         ,width: '5%'},
-            { data: 'dou'         ,width: '5%'},
-            { data: 'niti'        ,width: '5%'}
-        ],
-        language: {
-           url: "../static/main/js/japanese.json"
-        },
-        "scrollY":        "100px",
-        "pageLength": 1000,
-        searching: false,
-        info: false,
-        paging: false,
-        "order": [ 1, "asc" ],
-        "lengthMenu": [100, 300, 500, 1000],
-        dom:"<'row'<'col-sm-12'tr>>" +
-            "<'row'<'col-sm-6'l><'col-sm-6'f>>"+
-            "<'row'<'col-sm-5'i><'col-sm-7'p>>"
-    });
+    createDaichoTables_Main(rowData.id);
     
   } );
 
@@ -102,22 +58,67 @@ $("#chkMuko").change(function(){
 });
 
 
+function createDaichoTables_Main(customerId){
+    //台帳データテーブルを作成
+    $("#tableDaicho").DataTable({
+        bInfo: true,
+        bSort: false,
+        destroy: true,
+        "processing": true,
+        ajax: {
+            url: "/getVDaichoA_ByCusotmerId/" + customerId + "",
+            dataType: "json",
+            dataSrc: function ( json ) {
+                return JSON.parse(json.data);
+            },
+            contentType:"application/json; charset=utf-8",
+            complete: function () {
+                return; 
+            }
+        },
+        columns: [
+            { data: 'item_id'     ,width: '12%'},
+            { data: 'iname1'      ,width: '41%'},
+            { data: 'tanka'       ,width: '12%'},
+            { data: 'getu'        ,width: '5%'},
+            { data: 'ka'          ,width: '5%'},
+            { data: 'sui'         ,width: '5%'},
+            { data: 'moku'        ,width: '5%'},
+            { data: 'kin'         ,width: '5%'},
+            { data: 'dou'         ,width: '5%'},
+            { data: 'niti'        ,width: '5%'}
+        ],
+        columnDefs: [
+          {
+              targets: 2, className: 'dt-body-right',
+              targets: 3, className: 'dt-body-center',
+              targets: 4, className: 'dt-body-center',
+              targets: 5, className: 'dt-body-center',
+              targets: 6, className: 'dt-body-center',
+              targets: 7, className: 'dt-body-center',
+              targets: 8, className: 'dt-body-center',
+              targets: 9, className: 'dt-body-center'
+          }
+       ],
+        language: {
+           url: "../static/main/js/japanese.json"
+        },
+        "scrollY":        "100px",
+        "pageLength": 1000,
+        searching: false,
+        info: false,
+        paging: false,
+        "order": [ 1, "asc" ],
+        "lengthMenu": [100, 300, 500, 1000],
+        dom:"<'row'<'col-sm-12'tr>>" +
+            "<'row'<'col-sm-6'l><'col-sm-6'f>>"+
+            "<'row'<'col-sm-5'i><'col-sm-7'p>>"
+    });
+}
 
 
 
 function createCustomerTables_Main(){
-  //var domTableCustomer = $('#tableCustomer').DataTable();
-  //
-  //domTableCustomer = null;
-  //
-  //if (domTableCustomer) {
-  //    // 二回目以降の描画の場合、初期化が必要
-  //    //domTableCustomer.fnDestroy();
-  //    domTableCustomer.state.clear();
-  //    domTableCustomer.destroy();
-  //    $("#tableCustomer tbody > tr").remove();  // $("#result-table tbody").empty();で良い
-  //    $("#tableCustomer tbody").empty();
-  //}
     
   var groupkb = $("#selGroupKb").val();
   if(groupkb == undefined) {
@@ -157,6 +158,12 @@ function createCustomerTables_Main(){
           { data: 'list'   ,width: '25%'},
           { data: 'name1'  ,width: '50%'}
       ],
+      columnDefs: [
+        {
+            targets: 1,
+            className: 'dt-body-center'
+        }
+     ],
       language: {
          url: "../static/main/js/japanese.json"
       },
