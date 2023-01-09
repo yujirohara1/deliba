774d441919d8a0aa509c86bb69d6ea8c72800a84
yujirohara1 <35021147+yujirohara1@users.noreply.github.com>
2023-01-09 15:19:41 +0900

    a

--- app.py
+++ app.py
@@ -1145,7 +1145,7 @@ def resJson_getCsvData(viewnm, nentuki, groupkb, tanto):
   elif viewnm == "v_csv_uriage_kokyakubetu":
     sqlwhere = sqlwhere + " and nen = '" + nentuki[0:4] + "' and tuki = '" + nentuki[4:6] + "' and group_id = " + groupkb + " and tanto_id = '" + tanto + "' " 
   elif viewnm == "v_csv_hikiotosi":
-    sqlwhere = sqlwhere + " and nen = '" + nentuki[0:4] + "' and tuki = '" + nentuki[4:6] + "' " 
+    sqlwhere = " nen = '" + nentuki[0:4] + "' and tuki = '" + nentuki[4:6] + "' " 
   elif viewnm == "v_csv_takuhai":
     sqlwhere = sqlwhere + " and group_id = " + groupkb + " and tanto_id = '" + tanto + "' " 
   else:
